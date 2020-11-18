49ec4fc0df14208e72c423387bb3884bb2b53c8b
lenabeliaeva <lena.belia@yandex.ru>
2020-11-18 17:22:09 +0300

    added annotation to request body

--- app/src/main/java/com/example/surfedmemes/retrofit/LoginService.kt
+++ app/src/main/java/com/example/surfedmemes/retrofit/LoginService.kt
@@ -1,11 +1,12 @@
-package com.example.surfedmemes.`interface`
+package com.example.surfedmemes.retrofit
 
 import com.example.surfedmemes.model.LoginRequest
 import com.example.surfedmemes.model.LoginResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface LoginService {
     @POST("/auth/login")
-    fun authenticate(request: LoginRequest): Call<LoginResponse>
+    fun authenticate(@Body request: LoginRequest): Call<LoginResponse>
 }
