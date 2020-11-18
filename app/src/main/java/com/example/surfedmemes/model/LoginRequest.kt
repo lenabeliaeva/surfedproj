1b226e4d226674c75eea654e910b178edad0da9e
lenabeliaeva <lena.belia@yandex.ru>
2020-11-18 17:30:55 +0300

    added serializedName annotations

--- app/src/main/java/com/example/surfedmemes/model/LoginRequest.kt
+++ app/src/main/java/com/example/surfedmemes/model/LoginRequest.kt
@@ -1,6 +1,10 @@
 package com.example.surfedmemes.model
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginRequest (
+    @SerializedName("login")
     var login: String? = null,
+    @SerializedName("password")
     var password: String? = null
 )
--- app/src/main/java/com/example/surfedmemes/model/LoginResponse.kt
+++ app/src/main/java/com/example/surfedmemes/model/LoginResponse.kt
@@ -1,6 +1,10 @@
 package com.example.surfedmemes.model
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginResponse (
+    @SerializedName("accessToken")
     var accessToken: String? = null,
+    @SerializedName("userInfo")
     var userInfo: User? = null
 )
