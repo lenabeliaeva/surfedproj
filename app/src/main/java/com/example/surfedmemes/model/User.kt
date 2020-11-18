d0e76cc32b83f81e091e8e1cc957dbc9fd0da3ec
lenabeliaeva <lena.belia@yandex.ru>
2020-11-18 17:32:15 +0300

    corrected fields names

--- app/src/main/java/com/example/surfedmemes/model/User.kt
+++ app/src/main/java/com/example/surfedmemes/model/User.kt
@@ -2,7 +2,7 @@ package com.example.surfedmemes.model
 
 data class User(
     var id: Int? = null,
-    var name: String? = null,
+    var username: String? = null,
     var firstName: String? = null,
     var secondName: String? = null,
     var userDescription: String? = null
